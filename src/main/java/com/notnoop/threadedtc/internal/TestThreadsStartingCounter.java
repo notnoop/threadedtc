5f4113518c70691c22da792faa57b5d1a582d26a
notnoop <mahmood@notnoop.com>
2009-12-13 18:49:12 -0500

    adding LICENSE

--- src/main/java/com/notnoop/threadedtc/internal/TestThreadsStartingCounter.java
+++ src/main/java/com/notnoop/threadedtc/internal/TestThreadsStartingCounter.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2001-2009 Artima, Inc.
+ * Copyright 2009 Mahmood Ali
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
